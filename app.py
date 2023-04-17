d781465bb3a2635a3778b5886e52af1b9426e6c0
bibaf <zvi.fux1@gmail.com>
2023-04-17 18:39:11 +0300

    fix app.py

--- app.py
+++ app.py
@@ -24,28 +24,18 @@ def drop_list():
         results = collection.find({"affected.package.ecosystem": selected_option}, {'aliases': 1, 'published': 1, 'details': 1, 'summary': 1})
         results_list = list(results)
         results_str = str(results_list)
-        replace = results_str.replace('\n', '<br>').replace("_id", "\n_id").replace("\\n", "\n").replace("]", "").replace("-", "").replace("[", "").replace(",", "\n").replace("{", "").replace("}", "").replace("'", "")
-        with open("/app/results.txt") as file:
-            file_content = file.read()
+        replace = results_str.replace("_id", "\n_id").replace('###', "").replace('"', "").replace("]", "").replace("-", "").replace("[", "").replace(",", "\n").replace("{", "").replace("}", "").replace("'", "")
+        with open("/app/results.txt", "w") as file:
+            content = file.write(replace)
+            file.close()
+
+
+        with open("/app/results.txt") as final_file:
+            file_content = final_file.read()
             file_content = file_content.replace('\n', '<br>')
+            
         return render_template('results.html', file_content=file_content)
-        #html_markup = Markup(replace)
-        #with open("results.txt" ,"w") as file:
-        #    file.write(replace)
-        #    file.close()
-        
-        #return render_template_string(replace)
-        #return render_template('results.html', file_content=replace)
 
-
-
-
-
-
-        #return render_template('results.html', html=replace)
-
-
-    # Render the template with the drop-down list
     return render_template('index.html', options=options)
 
 if __name__ == '__main__':
@@ -55,10 +45,3 @@ if __name__ == '__main__':
 
 
 
-
-# db.advisories.find({"affected.package.ecosystem": "Go"}, {'published': 1, 'aliases': 1, 'summary': 1, 'details:': 1})
-
-
-#array = list(collection.find({"affected.package.ecosystem": "Go"}, {'published': 1, 'aliases': 1, 'summary': 1, 'details:': 1}))
-
-#print(array)
