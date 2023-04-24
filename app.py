bd6d4a482a0cc175d986d01ae79e4c61d1b8a046
bibaf <zvi.fux1@gmail.com>
2023-04-24 15:18:20 +0300

    fixes

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request, render_template, send_file
 from pymongo import MongoClient
-import json
-from markupsafe import Markup
-
+import requests
 
 app=Flask(__name__,template_folder='template')
 
-# Connect to MongoDB
+# Connect to MongoDB database
 client = MongoClient('mongodb://localhost:27017/')
-db = client['GHSA']  # Replace 'mydatabase' with your database name
+db = client['GHSA']
 collection = db['advisories']  # Replace 'mycollection' with your collection name
+
+
+
+
 
 
 
@@ -19,29 +23,15 @@ def drop_list():
     options = ['GitHub Actions', 'Go', 'Hex', 'Maven','NuGet', 'Packagist','Pub','PyPI', 'RubyGems', 'crates.io', 'npm']
 
     if request.method == 'POST':
-        # Get the selected option from the form submission
         selected_option = request.form.get('option')
         results = collection.find({"affected.package.ecosystem": selected_option}, {'aliases': 1, 'published': 1, 'details': 1, 'summary': 1})
-        results_list = list(results)
-        results_str = str(results_list)
-        replace = results_str.replace("_id", "\n_id").replace('###', "").replace('"', "").replace("]", "").replace("-", "").replace("[", "").replace(",", "\n").replace("{", "").replace("}", "").replace("'", "")
-        with open("/app/results.txt", "w") as file:
-            content = file.write(replace)
-            file.close()
+        return render_template('search.html', results=results)
 
-
-        with open("/app/results.txt") as final_file:
-            file_content = final_file.read()
-            file_content = file_content.replace('\n', '<br>')
-            
-        return render_template('results.html', file_content=file_content)
 
     return render_template('index.html', options=options)
 
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
-
-
-
